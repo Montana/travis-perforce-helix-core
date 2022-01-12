59f42a3de2aa42668be65ff7c82f0ae3e04a25ff
Montana Mendy <montana@linux.com>
2022-01-12 14:01:58 -0800

    Update perforce_script.sh

--- perforce_script.sh
+++ perforce_script.sh
@@ -4,8 +4,8 @@ set -x
 echo "Travis CI/Perfore-Helix shell script beginning" 
   
 wget -q https://package.perforce.com/perforce.pubkey -O - | sudo apt-key add -
-echo 'deb http://package.perforce.com/apt/ubuntu precise release' | sudo tee -a /etc/apt/sources.list"
-echo 'deb https://packagecloud.io/github/git-lfs/debian/ jessie main' | sudo tee -a /etc/apt/sources.list"
+"echo 'deb http://package.perforce.com/apt/ubuntu precise release' | sudo tee -a /etc/apt/sources.list"
+"echo 'deb https://packagecloud.io/github/git-lfs/debian/ jessie main' | sudo tee -a /etc/apt/sources.list"
 sudo apt-get install linuxbrew-wrapper
 sudo apt-get update -qq
 sudo apt-get install helix-p4d
