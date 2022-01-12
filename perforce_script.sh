2ac8e3fa7bf318d9c1f9b9d36f231cc741d76aae
Montana Mendy <montana@linux.com>
2022-01-12 13:58:38 -0800

    Update perforce_script.sh

--- perforce_script.sh
+++ perforce_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-set -x                                                                                                                                               "
+set -x                                                                                                                                               
 
 echo "Travis CI/Perfore-Helix shell script beginning" 
   
