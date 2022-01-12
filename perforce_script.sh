fdec8adcbf56768d3738720e95e37146f26dbe1f
Montana Mendy <montana@linux.com>
2022-01-12 14:18:17 -0800

    Update perforce_script.sh

--- perforce_script.sh
+++ perforce_script.sh
@@ -1,20 +1,33 @@
 #!/bin/bash 
-set -x                                                                                                                                               
+set -ex                                                                                                                                               
 
-echo "Travis CI/Perfore-Helix shell script beginning" 
-  
-wget -q https://package.perforce.com/perforce.pubkey -O - | sudo apt-key add -
-"echo 'deb http://package.perforce.com/apt/ubuntu precise release' | sudo tee -a /etc/apt/sources.list"
-"echo 'deb https://packagecloud.io/github/git-lfs/debian/ jessie main' | sudo tee -a /etc/apt/sources.list"
-sudo apt-get install linuxbrew-wrapper
-sudo apt-get update -qq
-sudo apt-get install helix-p4d
-sudo apt-get install -y apt-transport-https
-sudo systemctl enable helix-p4dctl
-sudo systemctl start helix-p4dctl
-sudo /opt/perforce/sbin/configure-helix-p4d.sh -n perforce1 -p ssl:1666 -r /opt/perforce/servers/perforce1 -u montana -P REinforce --unicode
-p4 info
-p4 branches
-p4 -V
-p4 -h
-p4 -size
+function update_packages()
+{
+    wget -q https://package.perforce.com/perforce.pubkey -O - | sudo apt-key add -
+    "echo 'deb http://package.perforce.com/apt/ubuntu precise release' | sudo tee -a /etc/apt/sources.list"
+    "echo 'deb https://packagecloud.io/github/git-lfs/debian/ jessie main' | sudo tee -a /etc/apt/sources.list"
+}
+
+
+function update_packages()
+{
+    sudo apt-get install linuxbrew-wrapper
+    sudo apt-get update -qq
+    sudo apt-get install helix-p4d
+    sudo apt-get install -y apt-transport-https
+    sudo systemctl enable helix-p4dctl
+    sudo systemctl start helix-p4dctl
+}
+
+#***********************************************************************************
+#	P4 Commands
+#***********************************************************************************
+function install_mandatories()
+{
+    p4 info
+    p4 branches
+    p4 -V
+    p4 -h
+    p4 -size
+
+}
