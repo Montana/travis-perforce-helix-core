6a5be43089f1ce0dab5c6193185653a0bce262e5
Montana Mendy <montana@linux.com>
2022-01-12 16:45:53 -0800

    Update perforce_script.sh

--- perforce_script.sh
+++ perforce_script.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
+set -x 
+set -xe 
+
 # Get the latest package lists
 
-apt-get update
+sudo apt-get update
 
 # Get DEB files
 
 wget -q https://package.perforce.com/perforce.pubkey -O - | sudo apt-key add -
-"echo 'deb http://package.perforce.com/apt/ubuntu precise release' | sudo tee -a /etc/apt/sources.list"
-"echo 'deb https://packagecloud.io/github/git-lfs/debian/ jessie main' | sudo tee -a /etc/apt/sources.list"
+echo 'deb http://package.perforce.com/apt/ubuntu precise release' | sudo tee -a /etc/apt/sources.list"
+echo 'deb https://packagecloud.io/github/git-lfs/debian/ jessie main' | sudo tee -a /etc/apt/sources.list"
 
 # Install from Repo
 
@@ -18,6 +21,10 @@ sudo apt-get install helix-p4d
 sudo apt-get install -y apt-transport-https
 sudo systemctl enable helix-p4dctl
 sudo systemctl start helix-p4dctl
+
+# Run P4 
+
+p4 info
 
 # Exit the script
 
